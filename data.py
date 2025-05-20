5a46b946d8f7d2442c42e138e9497b34d44fc0c7
Kike1322 <kikecumpiandiaz@gmail.com>
2025-05-20 15:31:30 +0200

    Update data.py

--- data.py
+++ data.py
@@ -2,7 +2,7 @@ import pandas as pd
 import ccxt
 
 def get_btc_data():
-    exchange = ccxt.binance()
+    exchange = ccxt.bybit()
     bars = exchange.fetch_ohlcv('BTC/USDT', timeframe='1h', limit=100)
     df = pd.DataFrame(bars, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
     df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
