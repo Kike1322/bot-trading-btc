e6c4bac7798c87008f47b33fca73716b04f1019b
Kike1322 <kikecumpiandiaz@gmail.com>
2025-05-21 16:01:02 +0200

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,8 +1,11 @@
-import csv
 import os
+import csv
 from datetime import datetime
 
-def log_trade(signal, amount, price, capital, colchon, beneficio_total, resultado="N/A"):
+def log_trade(signal, amount, price, capital, colchon, beneficio_total,
+              rsi=None, sma=None, ema=None, macd=None, macd_signal=None,
+              adx=None, stoch_k=None, stoch_d=None, resultado="N/A"):
+
     filename = "historial_trading.csv"
     file_exists = os.path.isfile(filename)
 
@@ -12,11 +15,15 @@ def log_trade(signal, amount, price, capital, colchon, beneficio_total, resultad
         if not file_exists:
             writer.writerow([
                 "fecha", "señal", "monto", "precio", "resultado",
-                "capital", "colchon", "beneficio_total"
+                "capital", "colchon", "beneficio_total",
+                "RSI", "SMA", "EMA", "MACD", "MACD_signal",
+                "ADX", "Stoch_K", "Stoch_D"
             ])
 
         writer.writerow([
             datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
             signal, amount, price, resultado,
-            round(capital, 2), round(colchon, 2), round(beneficio_total, 2)
+            round(capital, 2), round(colchon, 2), round(beneficio_total, 2),
+            rsi, sma, ema, macd, macd_signal,
+            adx, stoch_k, stoch_d
         ])
